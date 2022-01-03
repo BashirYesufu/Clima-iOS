501c1f008e8b177f575c0240764373ab3c339cd8
Bashir Yesufu <yesufubashir@gmail.com>
2022-01-03 13:21:33 +0100

    Validated textfield to guard against empty text entry

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -32,6 +32,15 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if !textField.text!.isEmpty {
+            return true
+        } else {
+            textField.placeholder = "Enter a valid city name"
+            return false
+        }
+    }
+    
     func textFieldDidEndEditing(_ textField: UITextField) {
         searchTextField.text = ""
     }
